93f1a793e4624444c2a40b985de30522671d788b
dev_jeehyeon <anna04143@gmail.com>
2022-07-25 23:46:33 +0900

    [initial] 스프링부트에 템플릿 적용한 것 최초 파일

---
+++ src/main/java/com/example/main/ICampDoItApplication.java
@@ -0,0 +1,15 @@
+package com.example.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan( basePackages = { "com.example.model1", "com.example.controller" } )
+public class ICampDoItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ICampDoItApplication.class, args);
+	}
+
+}
