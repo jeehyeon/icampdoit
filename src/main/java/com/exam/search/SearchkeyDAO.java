1c48dc395fdf4bf8e2d38213136f351763ebe815
soyeon <asyeon13@naver.com>
2022-08-10 05:58:25 +0900

    키워드검색 결과 리스트

--- src/main/java/com/exam/search/SearchkeyDAO.java
+++ src/main/java/com/exam/search/SearchkeyDAO.java
@@ -1,14 +1,17 @@
 package com.exam.search;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.stereotype.Repository;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -16,71 +19,95 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import com.exam.mboard.BoardListTO;
+import com.exam.mboard.BoardTO;
+
 @Repository
 public class SearchkeyDAO {
-	
-	private String getValue(String tag, Element element) {
-		// TODO Auto-generated method stub
-		if(element.getElementsByTagName(tag).item(0)== null) {
-			return " ";
-		}
-		NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
-		Node node =(Node) nodes.item(0);
 		
-		return node.getNodeValue();
-	}
-	
 	public List<SearchkeyTO> searchkeyDAO(String keysearch) {
+	      // TODO Auto-generated method stub
 		
-		try {
-			
-			DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
-			DocumentBuilder builder = factory.newDocumentBuilder();
-			
-			String str = "카라반";
-			//String str = request.getParameter( "keysearch" );
-			System.out.println( "keysearch : " + str );
-			
-			String keyword = URLEncoder.encode(str, "utf-8");
-			System.out.println( "keyword : " + keyword);
-			
-			String url = "http://api.visitkorea.or.kr/openapi/service/rest/GoCamping/searchList"
-					+ "?ServiceKey=74rd5r1MwtY%2F3dFG0s9I1UDtFBJBj1zjmm0VdZBNsJoslwPnviVh2PeV1vCg%2BtaHuMvN8G1f1PWIwKh3I%2BI0oQ%3D%3D"
-					+ "&numOfRows=3107"
-					+ "&pageNo=1"
-					+ "&MobileOS=ETC"
-					+ "&MobileApp=TestApp"
-					+ "&keyword=" + keyword;
-			
-			System.out.println( "url : " + url );
-						
-			Document document = builder.parse( url );
-			
-			NodeList nodes = document.getElementsByTagName( "item" );
-			for( int i=0 ; i<nodes.getLength() ; i++ ) {
-				Node node = nodes.item( i );
-				
-				if (node.getNodeType() == Node.ELEMENT_NODE) {
-                    Element element = (Element) node;
-                    System.out.println("캠핑장명 :" + getValue("facltNm", element));				
-					System.out.println("캠핑장 주소 :" + getValue("addr1", element)+" "+ getValue("addr2", element));
-					System.out.println("캠핑장 유형 :" + getValue("induty", element));
-				}
-			}
-			
-			
-		} catch (ParserConfigurationException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SAXException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		  ArrayList<SearchkeyTO> datas = new ArrayList<SearchkeyTO>();
+		
+	      try {
+	         DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();      
+	         DocumentBuilder builder = factory.newDocumentBuilder();
 
-		return null;
-	}
+	         String keyword = URLEncoder.encode( keysearch, "utf-8" );
+			 System.out.println( "keyword : " + keyword );
+	         
+	         Document document = builder.parse( new URL("https://api.visitkorea.or.kr/openapi/service/rest/GoCamping/searchList?ServiceKey=74rd5r1MwtY%2F3dFG0s9I1UDtFBJBj1zjmm0VdZBNsJoslwPnviVh2PeV1vCg%2BtaHuMvN8G1f1PWIwKh3I%2BI0oQ%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=TestApp&keyword=" +keyword).openStream());
+	         
+	         Element root = document.getDocumentElement();
+	         System.out.println( root.toString() );	// [response: null] 출력됨
+	         
+	         NodeList nodes = document.getElementsByTagName("item");
+	         //System.out.println( "1" );
+	         
+	         for ( int i = 0; i < nodes.getLength(); i++ ) {
+	            Node node = nodes.item(i);
+	            //System.out.println( "2" );
+	            
+	            if ( node.getNodeType() == Node.ELEMENT_NODE ) { // node객체가 앨리먼트인것만 화면에 출력
+	               Element element = (Element) node;	               
+	               //System.out.println( "3" );
+	               
+	               SearchkeyTO kto = new SearchkeyTO();
+	               kto.setFacltNm(getValue( "facltNm", element ));
+	               kto.setAddr1(getValue( "addr1", element ));
+	               kto.setInduty(getValue( "induty", element ));
+	               kto.setFirstImageUrl(getValue( "firstImageUrl", element ));
+	               
+	               
+	               kto.setContentId(getValue( "contentId", element ));
+	               /*
+	               kto.setIntro(getValue( "intro", element ));
+	               kto.setAnimalCmgCl(getValue( "animalCmgCl", element ));
+	               kto.setTrlerAcmpnyAt(getValue( "trlerAcmpnyAt", element ));
+	               kto.setCaravAcmpnyAt(getValue( "caravAcmpnyAt", element ));
+	               kto.setLctCl(getValue( "lctCl", element ));
+	               kto.setBrazierCl(getValue( "brazierCl", element ));
+	               kto.setSiteBottomCl1(getValue( "siteBottomCl1", element ));
+	               kto.setSiteBottomCl2(getValue( "siteBottomCl2", element ));
+	               kto.setSiteBottomCl3(getValue( "siteBottomCl3", element ));
+	               kto.setSiteBottomCl4(getValue( "siteBottomCl4", element ));
+	               kto.setSiteBottomCl5(getValue( "siteBottomCl5", element ));
+	               kto.setSbrsCl(getValue( "sbrsCl", element ));
+	               */
+	               
+	               //System.out.println( "4" );
+	               
+	               //System.out.println("캠핑장명 : " + getValue( "facltNm", element ));	
+	               //System.out.println("캠핑장 유형 : " + getValue( "induty", element ));
+	               //System.out.println("캠핑장 주소 : " + getValue( "addr1", element ));
+	               //System.out.println("캠핑장 대표사진 : " + getValue( "firstImageUrl", element ));
+	               
+	               datas.add(kto);
+	            }
+	         }
+	         
+	      } catch (MalformedURLException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      } catch (ParserConfigurationException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      } catch (SAXException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      } catch (IOException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	      
+	      return datas;
+	   }
+
+	   private static String getValue(String tag, Element element) {
+		   NodeList nodes = element.getElementsByTagName(tag).item(0).getChildNodes();
+		   Node node = (Node)nodes.item(0);
+		   return node.getNodeValue();
+	   }
 	
 }
--- src/main/java/com/exam/search/SearchkeyTO.java
+++ src/main/java/com/exam/search/SearchkeyTO.java
@@ -7,14 +7,24 @@ import lombok.Setter;
 @Setter
 public class SearchkeyTO {
 	
-	private String facltNm;	// 캠핑장명
-
-	private String addr1;	// 캠핑장주소 = addr1 + addr2
+	private String facltNm;			// 캠핑장명
+	private String addr1;			// 캠핑장주소 = addr1 + addr2
 	private String addr2;
-	
-	private String induty;	// 캠핑장유형
-	
+	private String induty;			// 캠핑장유형(주요시설)
 	private String firstImageUrl;	// 대표사진
+	private String keysearch;		// 검색키워드
+	private String contentId;		// 캠핑장아이디
+	private String intro;			// 캠핑장소개
+	private String animalCmgCl;		// 애완동물동반
+	private String trlerAcmpnyAt;	// 개인트레일러입장
+	private String caravAcmpnyAt;	// 개인카라반입장
+	private String lctCl;			// 입지
+	private String brazierCl;		// 화로대
+	private String siteBottomCl1;	// 바닥형태(1잔디 / 2파쇄석 / 3테크 / 4자갈 / 5맨흙 )
+	private String siteBottomCl2;
+	private String siteBottomCl3;
+	private String siteBottomCl4;
+	private String siteBottomCl5;
+	private String sbrsCl;			// 부대시설(캠핑장 시설정보)
 	
-	private String keysearch;
 }
