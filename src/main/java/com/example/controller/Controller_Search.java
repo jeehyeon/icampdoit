5e8ec4ae6928bea7ee5da2b75cd266e00fac528f
soyeon <asyeon13@naver.com>
2022-08-16 11:58:19 +0900

    일반검색컨트롤러 map부분 삭제

--- src/main/java/com/example/controller/Controller_Search.java
+++ src/main/java/com/example/controller/Controller_Search.java
@@ -16,7 +16,6 @@ import com.exam.search.CampviewCmtTO;
 import com.exam.search.SearchListTO;
 import com.exam.search.SearchkeyDAO;
 import com.exam.search.SearchkeyTO;
-import com.exam.search.SearchmapDAO;
 
 
 @RestController
@@ -24,9 +23,6 @@ public class Controller_Search {
 	
 	@Autowired
 	SearchkeyDAO kdao;
-	@Autowired
-	SearchmapDAO mdao;
-	
 	
 	@RequestMapping( value="/searchkey.do" )
 	public ModelAndView searchkey(HttpServletRequest request) {
@@ -133,11 +129,13 @@ public class Controller_Search {
 		String lctClBeach = "";
 		String lctClIsland = "";
 		if( (request.getParameter( "lctClMount" )!=null&&request.getParameter( "lctClMount" )!="") ) {
+			addurl +="lctClMount=" + request.getParameter("lctClMount") +"&";
 			lctClMount = request.getParameter( "lctClMount" );
 		} else {
 			lctClMount = request.getParameter( "lctClMount" );
 		}
 		if( (request.getParameter( "lctClForest" )!=null&&request.getParameter( "lctClForest" )!="")  ) {
+			addurl +="lctClForest=" + request.getParameter("lctClForest") +"&";
 			lctClForest = request.getParameter( "lctClForest" );
 		} else {
 			lctClForest = request.getParameter( "lctClForest" );
@@ -286,17 +284,6 @@ public class Controller_Search {
 		return modelAndView;
 	}
 	
-	@RequestMapping( value="/searchmap.do" )
-	public ModelAndView searchmap(HttpServletRequest request) {
-		System.out.println( "searchmap() 호출" );
-		
-
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName( "/search/search_map" );
-		
-		return modelAndView;
-	}
-	
 	@RequestMapping( value="/campview.do" )
 	public ModelAndView campview(HttpServletRequest request, HttpSession session) {
 		System.out.println( "campview() 호출" );
@@ -360,100 +347,6 @@ public class Controller_Search {
 		
 		flag=kdao.campViewCmtInsert(cto);
 		return Integer.toString(flag);
-	}
-	
-	
-	@RequestMapping( value="/searchmapsido.do" )
-	public JSONObject searchmapsido(HttpServletRequest request) {
-		System.out.println( "searchmapsido() 호출" );
-		
-		SearchkeyTO sto = new SearchkeyTO();
-		String sido = "";
-		
-		System.out.println( request.getParameter( "sidoVal" ) );
-		
-		if( request.getParameter( "sidoVal" ).equals("서울시" )) {
-			sto.setDoNm( "서울시" );
-			sido = "서울시";
-			
-		} else if( request.getParameter( "sidoVal" ).equals("인천시" ) ){
-			sto.setDoNm( "인천시" );
-			sido = "인천시";
-		} else {
-			sto.setDoNm( "경기도" );
-			sido = "경기도";
-		}
-		
-		System.out.println( "doNm : " + sto.getDoNm() );
-		
-		ArrayList<SearchkeyTO> list = (ArrayList<SearchkeyTO>)mdao.listsigunguNm( sto.getDoNm() );
-		
-		JSONObject result = new JSONObject();	
-		
-		JSONArray jsonArray = new JSONArray();
-		for( SearchkeyTO to : list ){
-			String sigunguNm = to.getSigunguNm();
-			
-			JSONObject jsonObject = new JSONObject();
-
-			if( !sigunguNm.equals("default") ) {
-				
-				jsonObject.put( "sigunguNm", sigunguNm );
-				jsonArray.add( jsonObject );
-			}
-		}
-		
-		result.put( "jsonArray", jsonArray ); //배열을 다시 json에 담음. => ajax 에서 jsonArray(배열)를 안받아줘서 json 으로 다시 넣음
-		
-		return result;
-	}
-	
-	@RequestMapping( value="/searchmapgugun.do" )
-	public JSONObject searchmapgugun(HttpServletRequest request) {
-		System.out.println( "searchmapgugun() 호출" );
-		
-		SearchkeyTO sto = new SearchkeyTO();
-
-		sto.setDoNm( request.getParameter( "sidoVal" ) );
-		sto.setSigunguNm( request.getParameter( "gugunVal" ) );
-		
-		ArrayList<SearchkeyTO> list = (ArrayList<SearchkeyTO>)mdao.listsearchmap(sto);
-		
-		JSONObject listresult = new JSONObject();	
-		
-		JSONArray jsonArray = new JSONArray();
-		for( SearchkeyTO to : list ){
-			String facltNm = to.getFacltNm();
-			String addr1 = to.getAddr1();
-			String addr2 = to.getAddr2();
-			String induty = to.getInduty();
-			String firstImageUrl = to.getFirstImageUrl();
-			String contentId = to.getContentId();
-			String mapX = to.getMapX();
-			String mapY = to.getMapY();
-			
-			JSONObject jsonObject = new JSONObject();
-				
-			jsonObject.put( "facltNm", facltNm );
-			jsonObject.put( "addr1", addr1 );
-			jsonObject.put( "addr2", addr2 );
-			jsonObject.put( "induty", induty );
-			jsonObject.put( "contentId", contentId );
-			jsonObject.put( "mapX", mapX );
-			jsonObject.put( "mapY", mapY );
-			
-			if( !firstImageUrl.equals( "default" ) ) {
-				jsonObject.put( "firstImageUrl", firstImageUrl );
-			} else {
-				jsonObject.put( "firstImageUrl", "./resources/bootstrap-5/html/img/noimage.svg" );
-			}
-			
-			jsonArray.add( jsonObject );
-		}
-		
-		listresult.put( "jsonArray", jsonArray ); //배열을 다시 json에 담음. => ajax 에서 jsonArray(배열)를 안받아줘서 json 으로 다시 넣음
-		
-		return listresult;
 	}
 	
 }
