d95efc6e3fc61e92f4da9c33c8c1a472714c0915
rina <wao8387@gmail.com>
2022-08-16 14:58:11 +0900

    메인 top5 캠핑장 리스트 수정중

---
+++ src/main/java/com/exam/home/HomeDAO.java
@@ -0,0 +1,39 @@
+package com.exam.home;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.exam.mboard.BoardTO;
+import com.exam.search.SearchkeyTO;
+
+
+@Repository
+public class HomeDAO {
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+	
+	// 캠핑장 정보
+	public ArrayList<SearchkeyTO> campInfo() {	
+			
+		String sql = "select go_api.contentId, facltNm, firstImageUrl, doNm, sigunguNm from go_api inner join campview where go_api.contentId = campview.contentId order by hit desc limit 0,5";		
+		ArrayList<SearchkeyTO> lists = (ArrayList<SearchkeyTO>)jdbcTemplate.query(
+				sql, new BeanPropertyRowMapper<SearchkeyTO>(SearchkeyTO.class) );
+				
+		return lists;
+	}
+	
+	/*
+	// 조회수
+	public HomeTO hitList(HomeTO to) {
+				
+		String sql = "select seq, contentId, hit from campview order by hit desc";
+		to = jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<HomeTO>(HomeTO.class), to.getSeq() );
+		
+		return to;		
+	}*/
+
+}
